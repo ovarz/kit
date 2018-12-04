5c0b8ac4364c624fdd6162e697caf186f0bfb2b6
ovarz <ovarz_03@yahoo.com>
2018-12-04 15:20:44 +0700

    update audience

--- page/audience.php
+++ page/audience.php
@@ -14,8 +14,8 @@
   
     <div class="ad-device">
   	  <ul>
-  	    <li>
-  	  	  <a href="javascript:void(0)" class="ad-device-box popup-full">
+  	    <li class="popup-full">
+  	  	  <a href="javascript:void(0)" class="popup-audience-antv">
   	  		<div class="adb-name">ANTV <i class="fas fa-chevron-right smooth"></i></div>
   	  		<div class="adb-stat">99.9M Viewer</div>
   	  		<div class="adb-source">Per month in comscore</div>
@@ -28,8 +28,8 @@
   	    
   	    
   	    
-  	    <li>
-  	  	  <a href="javascript:void(0)" class="ad-device-box popup-full">
+  	    <li class="popup-full">
+  	  	  <a href="javascript:void(0)" class="popup-audience-tvone">
   	  		<div class="adb-name">tvOne <i class="fas fa-chevron-right smooth"></i></div>
   	  		<div class="adb-stat">99.9M Subscription</div>
   	  		<div class="adb-source">Per month in comscore</div>
@@ -42,8 +42,8 @@
   	    
   	    
   	    
-  	    <li>
-  	  	  <a href="javascript:void(0)" class="ad-device-box popup-full">
+  	    <li class="popup-full">
+  	  	  <a href="javascript:void(0)" class="popup-audience-viva">
   	  		<div class="adb-name">VIVA.co.id <i class="fas fa-chevron-right smooth"></i></div>
   	  		<div class="adb-stat">99.9M User</div>
   	  		<div class="adb-source">Per month in comscore</div>
@@ -89,6 +89,173 @@
       	  </li>
       	  <?php } ?>
       	</ul>
+      </div>
+    </span>
+  </div>
+</div>
+
+ 	  	  
+  	  	  
+<div class="popup-full-content pfc-white" id="popup-audience-viva">
+  <a class="pfc-close content_center" href="javascript:void(0)">X</a>
+  <div class="pfc-box content_center">
+    <span>
+      <div class="audience-slide audience-slide-viva">
+      	
+      	<div class="aud-each">
+      	  <ul>
+      	  	<li class="aud-left content_center">
+     	  	  <span>
+      	  	    <div class="aud-title"><h3>Digital</h3></div>
+      	  	    <div class="aud-info">
+      	  	  	  <div class="aud-caption"><b>15 brands from VMB that span youth culture</b></div>
+      	  	  	  <div class="aud-list">
+      	  	  	    <ul>
+      	  	  	  	  <li>70% aged 18 to 34</li>
+      	  	  	  	  <li>60/40 male to female ratio</li>
+      	  	  	  	  <li>78% has a higher education degree</li>
+      	  	  	  	  <li>55% works full-time</li>
+      	  	  	  	  <li>1 in 6 loves everyday luxury</li>
+      	  	  	    </ul>
+      	  	  	  </div>
+      	  	    </div>
+      	  	  </span>
+      	  	</li>
+      	  	<li class="aud-right content_center">
+      	  	  <span>
+      	  	  	<div class="aud-digital-content">
+      	  	  	  <ul>
+     	  	  	    <?php for ($i=1; $i <= 15 ; $i++) { ?>
+      	  	  	  	<li>
+      	  	  	  	  <a href="javascript:void(0)" class="adc-button">
+      	  	  	  	  	<div class="adc-title">
+      	  	  	  	  	  <h4>
+      	  	  	  	  	  	<?php
+							  $a=array("CULTURE","DESIGN","FASHION","NEWS","MUSIC","GAMING");
+							  $random_keys=array_rand($a,2);
+							  echo $a[$random_keys[0]]."";
+						    ?>
+      	  	  	  	  	  </h4>
+      	  	  	  	  	</div>
+      	  	  	  	  	<div class="adc-box flex_thumb"><img src="img/sample/sample-<?php echo rand(1,10); ?>.jpg" /></div>
+      	  	  	  	  </a>
+      	  	  	  	  
+      	  	  	  	  <div class="aud-moreinfo">
+      	  	  	  	    <a class="audm-close content_center" href="javascript:void(0)">X</a>
+      	  	  	  	  	<div>
+      	  	  	  	  	  <div class="audm-cover flex_thumb">
+      	  	  	  	  	  	<img src="img/sample/sample-<?php echo rand(1,10); ?>.jpg" />
+      	  	  	  	  	  	<div class="audm-name content_center">
+      	  	  	  	  	  	  <h4>
+								<?php
+								  $a=array("CULTURE","DESIGN","FASHION","NEWS","MUSIC","GAMING");
+								  $random_keys=array_rand($a,2);
+								  echo $a[$random_keys[0]]."";
+								?>
+     	  	  	  	  	  	  </h4>
+      	  	  	  	  	  	</div>
+      	  	  	  	  	  </div>
+      	  	  	  	  	  <div class="audm-caption"><h5>Lorem ipsum dolor sit amet</h5></div>
+      	  	  	  	  	  <div class="audm-info">
+      	  	  	  	  	  	<ul>
+      	  	  	  	  	  	  <li class="audm-desc">
+      	  	  	  	  	  	  	<p>VIVA is a home for the most passionate of those gamers, providing both a home for community engagement, alongside thoughtful criticism and analysis built by and for the most knowledgeable of gamers.</p>
+      	  	  	  	  	  	  	<a href="https://viva.co.id" target="_blank">Visit Website &gt;</a>
+      	  	  	  	  	  	  </li>
+      	  	  	  	  	  	  <li class="audm-stat">
+      	  	  	  	  	  	    <div class="audm-stat-each">
+      	  	  	  	  	  	      <h6>Age</h6>
+      	  	  	  	  	  	      <ul>
+      	  	  	  	  	  	      	<li>
+      	  	  	  	  	  	      	  <span>18-25</span> <b>24%</b>
+      	  	  	  	  	  	      	</li>
+      	  	  	  	  	  	      	<li>
+      	  	  	  	  	  	      	  <span>25-45</span> <b>56%</b>
+      	  	  	  	  	  	      	</li>
+      	  	  	  	  	  	      </ul>
+      	  	  	  	  	  	    </div>
+      	  	  	  	  	  	    <div class="audm-stat-each">
+      	  	  	  	  	  	      <h6>Gender</h6>
+      	  	  	  	  	  	      <ul>
+      	  	  	  	  	  	      	<li>
+      	  	  	  	  	  	      	  <span>Male</span> <b>90%</b>
+      	  	  	  	  	  	      	</li>
+      	  	  	  	  	  	      	<li>
+      	  	  	  	  	  	      	  <span>Female</span> <b>10%</b>
+      	  	  	  	  	  	      	</li>
+      	  	  	  	  	  	      </ul>
+      	  	  	  	  	  	    </div>
+      	  	  	  	  	  	  	<a href="https://viva.co.id" class="btn" target="_blank">Download Media Kit</a>
+      	  	  	  	  	  	  </li>
+      	  	  	  	  	  	</ul>
+      	  	  	  	  	  </div>
+      	  	  	  	  	</div>
+      	  	  	  	  </div>
+      	  	  	  	</li>
+      	  	  	  	<?php } ?>
+      	  	  	  </ul>
+      	  	  	</div>
+      	  	  </span>
+			</li>
+      	  </ul>
+      	</div>
+      	
+      	
+      	
+      	<div class="aud-each">
+      	  <ul>
+      	  	<li class="aud-left content_center">
+     	  	  <span>
+      	  	    <div class="aud-title"><h3>Social</h3></div>
+      	  	    <div class="aud-info">
+      	  	  	  <div class="aud-caption"><b>VMB has strong partnerships with all major social media platforms, which are leveraged to ensure that we reach highly engaged audiences at scale.</b></div>
+      	  	  	  <div class="aud-list">
+      	  	  	    <ul>
+      	  	  	  	  <li>Facebook Anthology</li>
+      	  	  	  	  <li>YouTube DFP Cross-Sell</li>
+      	  	  	  	  <li>Snapchat Discover</li>
+      	  	  	  	  <li>Twitter Amplify and Live Premium</li>
+      	  	  	    </ul>
+      	  	  	  </div>
+      	  	    </div>
+      	  	  </span>
+      	  	</li>
+      	  	<li class="aud-right content_center">
+      	  	  <span>
+      	  	  	<div class="aud-social-content">
+      	  	  	  <ul>
+      	  	  	  	<li>
+      	  	  	  	  <div class="asc-box asc-fb content_center">
+      	  	  	  	  	<span><i class="fab fa-facebook-f"></i></span>
+      	  	  	  	  </div>
+      	  	  	  	</li>
+      	  	  	  	<li>
+      	  	  	  	  <div class="asc-box asc-tw content_center">
+      	  	  	  	  	<span><i class="fab fa-twitter"></i></span>
+      	  	  	  	  </div>
+      	  	  	  	</li>
+      	  	  	  	<li>
+      	  	  	  	  <div class="asc-box asc-ig content_center">
+      	  	  	  	  	<span><i class="fab fa-instagram"></i></span>
+      	  	  	  	  </div>
+      	  	  	  	</li>
+      	  	  	  	<li>
+      	  	  	  	  <div class="asc-box asc-yt content_center">
+      	  	  	  	  	<span><i class="fab fa-youtube"></i></span>
+      	  	  	  	  </div>
+      	  	  	  	</li>
+      	  	  	  	<li>
+      	  	  	  	  <div class="asc-box asc-sc content_center">
+      	  	  	  	  	<span><i class="fab fa-snapchat-ghost"></i></span>
+      	  	  	  	  </div>
+      	  	  	  	</li>
+      	  	  	  </ul>
+      	  	  	</div>
+      	  	  </span>
+			</li>
+      	  </ul>
+      	</div>
+      	
       </div>
     </span>
   </div>
